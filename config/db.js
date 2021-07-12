0a16fe870aa1e3fdd6e28fd88c131e2b080b9f4a
Eigel <eigelasinas1962@gmail.com>
2021-07-12 13:43:18 +0800

    remove commented-out lines

--- config/db.js
+++ config/db.js
@@ -1,23 +1,12 @@
 "use strict"
 
-const mysql      = require('mysql');
+const mysql  = require('mysql');
 const db = mysql.createConnection({
-    // port: 8080,
-  host     : '192.168.64.3',
-  user     : 'me',
+  host : '192.168.64.3',
+  user : 'me',
   password : '',
   database : 'billing_db',
   multipleStatements: true
 });
 
 module.exports = db
- 
-// connection.connect();
- 
-// connection.query('SELECT * FROM My_Table', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results)
-// //   console.log('The solution is: ', results[0].solution);
-// });
- 
-// connection.end();
